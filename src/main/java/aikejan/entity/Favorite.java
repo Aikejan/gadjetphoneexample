e3a630f532dfe33dfd303ad92df3795b83d293f2
aikezhanakhmatova <alinaakhmatova77@gmail.com>
2023-09-16 18:32:45 +0600

    Gadjet

--- src/main/java/aikejan/entity/Favorite.java
+++ src/main/java/aikejan/entity/Favorite.java
@@ -14,6 +14,7 @@ import static jakarta.persistence.CascadeType.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+
 public class Favorite {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE,
